7eb890735dd5f34287dbd1ff48a4f0e8771e954d
vishytheswishy <vishaal.yalaman@gmail.com>
2020-06-09 02:13:55 -0700

    fixed axis bug

--- graphics.py
+++ graphics.py
@@ -1,18 +1,20 @@
 import matplotlib.pyplot as plt
 with open('data.txt') as f:
     lines = f.readlines()
-    x = [line.split()[0] for line in lines]
-    y1 = [line.split()[1] for line in lines]
-    y2 = [line.split()[2] for line in lines]
+    x = [int(line.split()[0]) for line in lines]
+    y1 = [int(line.split()[1]) for line in lines]
+    y2 = [int(line.split()[2]) for line in lines]
 
-print(x)
-print(y1)
-print(y2)
-
+# print(x)
+# print(y1)
+# print(y2)
+print(max(x))
 fig = plt.figure()
 
 ax1 = fig.add_subplot(111)
 
+ax1.set_xlim([0,int(max(x))])
+ax1.set_ylim([0,int(max([max(y1), max(y2)]))])
 ax1.set_title("number of cells over steps(time)")    
 ax1.set_xlabel('steps')
 ax1.set_ylabel('num of cells')
